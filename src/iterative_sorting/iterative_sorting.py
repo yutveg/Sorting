dddbfd0d862cc85f6818372819b0dbd18392a16a
yutveg <kodi.0n31l@gmail.com>
2020-04-13 14:10:46 -0700

    Select sort done bubble sort needs conditions to repeat

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,39 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+# TO-DO: Complete the selection_sort() function below
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
+        # (hint, can do in 3 loc)
+        for j in range(i, len(arr)):
+            if(arr[j] < arr[i]):
+                current_val = arr[i]
+                arr[i] = arr[j]
+                arr[j] = current_val
         # TO-DO: swap
 
-
-
-
     return arr
-
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+
+
+def bubble_sort(arr):
+    swapped = False
+
+    for i in range(1, len(arr)):
+        if(arr[i] < arr[i - 1]):
+            swapped = True
+            cur_val = arr[i]
+            arr[i] = arr[i - 1]
+            arr[i - 1] = cur_val
 
     return arr
 
 
+print(bubble_sort([1, 9, 3, 4, 7, 8, 2]))
+
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+
+
+def count_sort(arr, maximum=-1):
 
     return arr
